6b488671994b4701ce71d6345c6ec729d4523630
okyanawang <okyanawang@gmail.com>
2023-06-01 14:07:59 +0700

    toJavanese API using express worked

---
+++ routes/index.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var router = express.Router();
+// import { toJavanese } from 'carakanjs';
+const { toJavanese } = require('carakanjs');
+
+/* GET home page. */
+router.get('/', function (req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.post('/tojavanese', function (req, res) {
+  var postData = req.body;
+  var javaneseText = toJavanese(postData.text);
+  res.json({ result: javaneseText });
+});
+
+module.exports = router;
