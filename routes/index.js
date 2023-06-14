733990f676422058473bd328527ef17dd96234ff
okyanawang <okyanawang@gmail.com>
2023-06-14 18:02:31 +0700

    Handle new line, added new route reverse translate

--- routes/index.js
+++ routes/index.js
@@ -8,6 +8,27 @@ router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.post('/revtojavanese', function (req, res) {
+  var postData = req.body;
+  var sections = postData.text.split(/\\n/); // Split postData into sections using \n as the delimiter
+
+  var javaneseText = '';
+  for (var i = 0; i < sections.length; i++) {
+    var section = sections[i];
+    var javaneseSection = toJavanese(section, {
+      useAccents: true,
+      useSwara: false,
+      useMurda: false,
+    });
+    javaneseText += javaneseSection;
+    if (i < sections.length - 1) {
+      javaneseText += '\n'; // Append \n delimiter if it's not the last section
+    }
+  }
+
+  res.json({ hasil: javaneseText });
+});
+
 router.post('/tojavanese', function (req, res) {
   var postData = req.body;
   var javaneseText = toJavanese(postData.text, {
