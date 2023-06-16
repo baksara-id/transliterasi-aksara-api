90b214f5fc1e356781de52ae4f0248f7b778697e
okyanawang <okyanawang@gmail.com>
2023-06-16 15:29:05 +0700

    Landing page message modified

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ const { toJavanese } = require('carakanjs');
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Aksara Jawa Translation API' });
 });
 
 router.post('/reversetrans', function (req, res) {
